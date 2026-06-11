07017e9f32880d819f6b31769cde2b64ce2677e0
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-06-11 13:42:20 +0200

    style: format files

--- crm_epic_events/models/database.py
+++ crm_epic_events/models/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, declarative_base
+from sqlalchemy.orm import declarative_base
 
 from crm_epic_events.utils.config import Config
 
