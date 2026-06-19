d023587c6aeb1b1ee2abf9470820412372831283
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-06-19 11:57:23 +0200

    feat(utils.printers.py): create functions to print text with colors in CLI

--- crm_epic_events/utils/__init__.py
+++ crm_epic_events/utils/__init__.py
@@ -1,2 +1,3 @@
 from .config import Config
-from .constants import Roles
+from .constants import Roles, GenericMessages
+from .printers import print_title, print_info, print_option, print_success, print_error, prompt
