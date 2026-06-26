900cd086c3acdec8e9b2ad9e27f5eface6f2274f
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-06-26 17:06:07 +0200

    fix(auth schema): add field_serializer to get correct format expected by JWT lib

--- crm_epic_events/services/authentication/schemas.py
+++ crm_epic_events/services/authentication/schemas.py
@@ -1,21 +1,31 @@
-# schemas.py (dans ton module authentication)
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_serializer
 
 from crm_epic_events.utils import Roles
 
 
-class AccessTokenPayload(BaseModel):
+class TokenBase(BaseModel):
     id: UUID
+    exp: datetime
+
+    @field_serializer("exp")
+    def serialize_exp(self, value: datetime) -> int:
+        # PyJWT expect timestamp, not datetime
+        return int(value.timestamp())
+
+    @field_serializer("id")
+    def serialize_id(self, value: UUID) -> str:
+        return str(value)
+
+
+class AccessTokenPayload(TokenBase):
     email: str
     role: Roles
-    exp: datetime
 
-    model_config = {"use_enum_values": True}  # sérialise Roles en str dans le JWT
+    model_config = {"use_enum_values": True}
 
 
-class RefreshTokenPayload(BaseModel):
-    id: UUID
-    exp: datetime
+class RefreshTokenPayload(TokenBase):
+    pass
