1cc322af81d9790350fe831f4a64d9329a1eebf9
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-06-26 16:35:34 +0200

    feat(custom errors): create custom errors

---
+++ crm_epic_events/errors/authentication_errors.py
@@ -0,0 +1,17 @@
+class CustomAuthenticationError(Exception):
+    def __init__(self, message: str = "Authentication error.", status_code: int = 401):
+        super().__init__(message)
+        self.status_code = status_code
+        self.message = message
+
+
+class CustomInvalidCredentialsError(CustomAuthenticationError):
+    def __init__(self, **kwargs):
+        kwargs.setdefault("message", "Invalid credentials")
+        super().__init__(**kwargs)
+
+
+class CustomInvalidTokenError(CustomAuthenticationError):
+    def __init__(self, **kwargs):
+        kwargs.setdefault("message", "Invalid token")
+        super().__init__(**kwargs)
