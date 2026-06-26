d29af92374da193b7d724f3c0e063f9d6e6ed9b9
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-06-26 12:01:27 +0200

    feat(main controller): update controller logic to handle what is displayed and call new utils methods

--- crm_epic_events/controllers/main.py
+++ crm_epic_events/controllers/main.py
@@ -1,4 +1,4 @@
-from crm_epic_events.utils import GenericMessages, StandardInputs
+from crm_epic_events.utils import MenuItem, StandardInputs, check_choice, exit_app
 from crm_epic_events.views import MainMenuView
 
 
@@ -7,63 +7,36 @@ class MainController:
 
     def __init__(self):
         self.view = MainMenuView()
-        # self.menu_actions = {
-        #     "1": self.handle_contracts_menu,
-        #     "2": self.handle_customers_menu,
-        #     "3": self.handle_events_menu,
-        #     StandardInputs.CANCELLED: self.exit_app,
-        # }
+        self.menu_items = [
+            MenuItem("1", "Manage Customers  👥", self.handle_customers_menu),
+            MenuItem("2", "Manage Contracts  📄", self.handle_contracts_menu),
+            MenuItem("3", "Manage Events     📅", self.handle_events_menu),
+            MenuItem(StandardInputs.CANCELLED, "Quit", self.exit_app),
+        ]
 
     def handle_main_menu(self):
-        """
-        """
-        # Todo: keep while true logic ?
+        """ """
+        # Todo: first check if user is auth, else display login screen
         while True:
+            choice = self.view.display(self.menu_items)
+            item = check_choice(choice, self.menu_items)
+            if item is None:
+                continue
+            else:
+                item.action()
 
-            #Todo: first check if user is auth, else display login screen
+    def handle_customers_menu(self):
+        pass
 
-            choice = self.view.display()
-            # action = self.menu_actions.get(choice)
-            # if action:
-            #     # action() return True to stay in this menu or False to got back to main menu
-            #     should_we_stay_in_this_menu = action()
-            #     if not should_we_stay_in_this_menu:
-            #         return None
-            # else:
-            #     # no error raising here to stay in this menu and avoid redirection to main menu
-            #     print_invalid_option(
-            #         [key for key in self.menu_actions.keys()], False, GenericMessages.MAIN_MENU_RETURN
-            #     )
+    def handle_contracts_menu(self):
+        pass
 
-    # def handle_player_menu(self) -> True:
-    #     """
-    #     Display PlayerController menu screen.
-    #     No handle errors here, as it is done in PlayerController.
-    #
-    #     Returns:
-    #         bool: Always returns True. This is designed to ensure continuity of
-    #         program operations even in the event of an exception.
-    #     """
-    #     player_controller = PlayerController(self.data)
-    #     player_controller.handle_player_main_menu()
-    #     return True
-    #
-    # def handle_tournament_menu(self) -> True:
-    #     """
-    #     Display TournamentController menu screen.
-    #     No handle errors here, as it is done in TournamentController.
-    #
-    #     Returns:
-    #         bool: Always returns True. This is designed to ensure continuity of
-    #         program operations even in the event of an exception.
-    #     """
-    #     tournament_controller = TournamentController(self.data)
-    #     tournament_controller.handle_tournament_main_menu()
-    #     return True
+    def handle_events_menu(self):
+        pass
 
-    def exit_app(self) -> None:
+    @staticmethod
+    def exit_app() -> None:
         """
         Prints an exit message and terminates the application.
         """
-        print(GenericMessages.EXIT)
-        exit(0)
+        exit_app()
