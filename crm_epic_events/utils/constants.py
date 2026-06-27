e862685659056e686833804ed60cd8557f309aa9
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-06-27 12:46:35 +0200

    feat(constants): create an enum of permissions by roles and objects: each action on an object is a member, its value are the list of roles allowed

--- crm_epic_events/utils/constants.py
+++ crm_epic_events/utils/constants.py
@@ -10,6 +10,30 @@ class Roles(StrEnum):
     SALES = "sales"
 
 
+class Permissions(list[Roles], Enum):
+    # Reminder from specifications: all roles can read all objects
+
+    # --- Users ---
+    USER_CREATE = [Roles.MANAGER]
+    USER_UPDATE = [Roles.MANAGER]
+    USER_DELETE = [Roles.MANAGER]
+
+    # --- Customers ---
+    CUSTOMER_CREATE = [Roles.SALES]
+    CUSTOMER_UPDATE = [Roles.MANAGER, Roles.SALES]  # SALES: only their own customers (ownership check)
+    CUSTOMER_DELETE = [Roles.MANAGER]
+
+    # --- Contracts ---
+    CONTRACT_CREATE = [Roles.MANAGER]
+    CONTRACT_UPDATE = [Roles.MANAGER, Roles.SALES]  # SALES: only contracts of their own customers (ownership check)
+    CONTRACT_DELETE = [Roles.MANAGER]
+
+    # --- Events ---
+    EVENT_CREATE = [Roles.SALES]  # SALES: only for their customers with a signed contract (ownership check)
+    EVENT_UPDATE = [Roles.MANAGER, Roles.SUPPORT]  # SUPPORT: only their own events (ownership check)
+    EVENT_DELETE = [Roles.MANAGER]
+
+
 class GenericMessages(StrEnum):
     EXIT = "Exiting the program."
     MAIN_MENU_RETURN = "Go back to Main menu in ... "
