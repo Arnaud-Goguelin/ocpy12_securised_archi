5c7dda68cbce02f8b21d3b692bc53a3da1e88a22
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-07-01 16:15:23 +0200

    feat(__init__): create many __init__ file to facilitate imports

---
+++ crm_epic_events/permissions/__init__.py
@@ -0,0 +1,1 @@
+from .roles_permissions import require_roles
--- crm_epic_events/utils/__init__.py
+++ crm_epic_events/utils/__init__.py
@@ -2,3 +2,4 @@ from .constants import Roles, GenericMessages, StandardInputs, NavSignal, MenuIt
 from .printers import print_title, print_info, print_option, print_success, print_unexpected_error, prompt
 from .check_choice import check_choice
 from .exit_app import exit_app
+from .db_transaction import db_transaction
