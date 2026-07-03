db9c045aa92a4622e03775b486df8563fd647058
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-07-03 12:58:53 +0200

    git add (customer service): implement customer pydantic shcema and service to manager CRUD operation on Customer object

---
+++ crm_epic_events/services/customer/schemas.py
@@ -0,0 +1,42 @@
+import uuid
+
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+
+
+# --- Input schemas ---
+
+
+class CustomerCreateInput(BaseModel):
+    salesperson_id: uuid.UUID
+    company_vat: str
+    company_name: str | None = None  # used for auto-creation if company not found
+    email: EmailStr
+    first_name: str
+    last_name: str
+    phone: str
+
+
+class CustomerUpdateInput(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    email: EmailStr | None = None
+    phone: str | None = None
+    company_name: str | None = None
+
+
+# --- Output schema ---
+
+
+class CustomerResponse(BaseModel):
+    id: uuid.UUID
+    first_name: str
+    last_name: str
+    email: str
+    phone: str
+    company_name: str
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = {"from_attributes": True}
