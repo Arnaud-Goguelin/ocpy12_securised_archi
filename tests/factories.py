6114faf1d76b12fb62ea4dbe7bd7b2a38e005a50
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-07-03 16:19:40 +0200

    tests(contract service): implement tests for contract service

--- tests/factories.py
+++ tests/factories.py
@@ -3,7 +3,7 @@ import factory
 
 from faker import Faker
 
-from crm_epic_events.models import Company, Customer, User
+from crm_epic_events.models import Company, Contract, Customer, User
 from crm_epic_events.utils import Roles
 
 
@@ -40,3 +40,15 @@ class CustomerFactory(factory.Factory):
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
     phone = factory.Faker("phone_number")
+
+
+class ContractFactory(factory.Factory):
+    class Meta:
+        model = Contract
+
+    id = factory.Faker("uuid4")
+    customer_id = factory.Faker("uuid4")
+    salesperson_id = factory.Faker("uuid4")
+    total_amount = factory.Faker("pyfloat", min_value=100, max_value=100_000, right_digits=2, positive=True)
+    remaining_amount = factory.LazyAttribute(lambda o: round(o.total_amount / 2, 2))
+    status = False
