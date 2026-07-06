96cde7fe90af40722176f1f5721ec018d1e00ab9
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-07-06 14:43:28 +0200

    refactor(company service): delete useless role and ownship checks

--- crm_epic_events/services/company/service.py
+++ crm_epic_events/services/company/service.py
@@ -2,16 +2,15 @@ import uuid
 
 from typing import TYPE_CHECKING
 
-from crm_epic_events.errors import UserNotAllowedError
+from crm_epic_events.errors import CompanyAlreadyExistsError
 from crm_epic_events.models import Company
 from crm_epic_events.services.customer.schemas import CustomerCreateInput
-from crm_epic_events.utils import Roles, db_transaction
+from crm_epic_events.utils import db_transaction
 
 
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
-    from crm_epic_events.models import User
     from crm_epic_events.services.company.schemas import CompanyCreateInput, CompanyUpdateInput
 
 
@@ -29,37 +28,24 @@ class CompanyService:
         return Company.get_by_vat(vat_number, db)
 
     @staticmethod
-    def create(current_user: "User", data: "CompanyCreateInput | CustomerCreateInput", db: "Session") -> "Company":
-        """MANAGER and SALES can create a company."""
-        if current_user.role not in (Roles.MANAGER, Roles.SALES):
-            raise UserNotAllowedError()
-
+    def create(data: "CompanyCreateInput | CustomerCreateInput", db: "Session") -> "Company":
         existing = Company.get_by_vat(data.vat_number, db)
         if existing:
-            raise ValueError(f"Company with VAT '{data.vat_number}' already exists.")
+            raise CompanyAlreadyExistsError()
 
         with db_transaction(db, "Creating company"):
             return Company.create(data.vat_number, data.name, db)
 
     @staticmethod
     def update(
-        current_user: "User",
         target_company: "Company",
         data: "CompanyUpdateInput",
         db: "Session",
     ) -> "Company":
-        """MANAGER and SALES can update a company."""
-        if current_user.role not in (Roles.MANAGER, Roles.SALES):
-            raise UserNotAllowedError()
-
         with db_transaction(db, "Updating company"):
             return target_company.update(data, db)
 
     @staticmethod
-    def delete(current_user: "User", target_company: "Company", db: "Session") -> None:
-        """Only MANAGER can delete a company."""
-        if current_user.role != Roles.MANAGER:
-            raise UserNotAllowedError()
-
+    def delete(target_company: "Company", db: "Session") -> None:
         with db_transaction(db, "Deleting company"):
             target_company.delete(db)
