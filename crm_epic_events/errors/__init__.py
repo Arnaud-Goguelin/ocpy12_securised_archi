79dbf1dd40bad972404b47ab78e6403846c84afc
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-07-06 13:51:25 +0200

    feat(errors): implement new CustomUserError

--- crm_epic_events/errors/__init__.py
+++ crm_epic_events/errors/__init__.py
@@ -1,2 +1,8 @@
-from .authentication_errors import CustomAuthenticationError, CustomInvalidCredentialsError, CustomInvalidTokenError, UserNotAuthenticatedError
+from .authentication_errors import (
+    CustomAuthenticationError,
+    CustomInvalidCredentialsError,
+    CustomInvalidTokenError,
+    UserNotAuthenticatedError,
+    )
+from .user_errors import UserAlreadyExistsError
 from .permissions_errors import UserNotAllowedError, UserIsNotOwnerError
---
+++ crm_epic_events/errors/user_errors.py
@@ -0,0 +1,11 @@
+class CustomUserError(Exception):
+    def __init__(self, message: str = "User error.", status_code: int = 400):
+        super().__init__(message)
+        self.status_code = status_code
+        self.message = message
+
+
+class UserAlreadyExistsError(CustomUserError):
+    def __init__(self, **kwargs):
+        kwargs.setdefault("message", "A user with this email already exists.")
+        super().__init__(**kwargs)
