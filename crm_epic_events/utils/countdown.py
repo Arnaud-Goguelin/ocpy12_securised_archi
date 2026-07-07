8923b85914eac87a43d30e238c0625a897091f3f
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2026-07-07 15:25:07 +0200

    feat(coutdown): allow a custom timer

--- crm_epic_events/utils/countdown.py
+++ crm_epic_events/utils/countdown.py
@@ -3,11 +3,11 @@ import time
 from .constants import GenericMessages
 
 
-def countdown(message: GenericMessages):
+def countdown(message: GenericMessages, timer: int = 3):
     """
     Counts down from 3 to 0, displaying a message before each number.
     """
-    for i in range(3, -1, -1):
+    for i in range(timer, -1, -1):
         print(f"\r{message.value}{i}", end="", flush=True)
         time.sleep(1)
     print("\n")
